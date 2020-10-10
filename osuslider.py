dfc1b8488568ad2beacc5f5483a4f6035a85caef
Oderjunkie <58880677+Oderjunkie@users.noreply.github.com>
2020-10-10 21:02:07 +0300

    Preparing for optimized drawing routine

--- osuslider.py
+++ osuslider.py
@@ -5,7 +5,7 @@ class Slider(OsuObject, pg.sprite.Sprite):
         y = int(obj[1])
         sound = int(obj[4])
         t = int(obj[2])
-        timing = [i for i in settings.importing.timingpoints if (int(i[0])<=t and i[6]=='1')].pop()
+        timing = [i for i in settings.importing.timingpoints if (int(i[0])<t and i[6]=='1')].pop()
         timing = timing[1]
         length = int(obj[7])
         self.anchor = [i.split(':') for i in obj[5][2:].split('|')]
@@ -29,17 +29,21 @@ class Slider(OsuObject, pg.sprite.Sprite):
         number = pg.transform.scale(self.images[f'default-{(self.num%9)+1}'].copy(), (int(cs/4), int(52*cs/128)))
         alpha = self.opac(objar, last, mills) * 255
         hitcircle.fill(tuple(self.color), special_flags=pg.BLEND_RGB_MULT)
-        screen = pg.Surface([640, 480], pg.SRCALPHA, 32)
-        screen = screen.convert_alpha()
         pos = [int(int(i)+cs) for i in self.anchor[-1]]
-        pg.draw.line(screen, (self.color[0], self.color[1], self.color[2]), [int(self.x+cs), int(self.y+cs)], pos, cs)
+        pos = [pos[0]-int(self.x), pos[1]-int(self.y)]
+        offset = [-min(pos[0], 0), -min(pos[1], 0)]
+        screen = pg.Surface([int(abs(pos[0])+3*cs), int(abs(pos[1])+3*cs)], pg.SRCALPHA, 32)
+        pos = [pos[0]+offset[0], pos[1]+offset[1]]
+        screen = screen.convert_alpha()
+        realoff = [3*cs/2, 3*cs/2]
+        pg.draw.line(screen, self.color, [int(offset[0]+cs+realoff[0]), int(offset[1]+cs+realoff[1])], [pos[0]+3*cs/2, pos[1]+3*cs/2], cs)
         [screen.blit(*i) for i in [\
-                     [big_boi, [int((self.x-cs*size/2)+cs), int((self.y-cs*size/2)+cs)]],\
-                     [hitcircle, [int(self.x+cs/2), int(self.y+cs/2)]],\
-                     [hitcircleoverlay, [int(self.x+cs/2), int(self.y+cs/2)]],\
-                     [number, [int(self.x+(3*cs/8)+cs/2), int(self.y+(76*cs/256)+cs/2)]],\
-                     [hitcircle, [int(pos[0]-cs/2),int(pos[1]-cs/2)]],\
-                     [hitcircleoverlay, [int(pos[0]-cs/2),int(pos[1]-cs/2)]]\
+                     [hitcircle, [int(offset[0]+cs/2+realoff[0]), int(offset[1]+cs/2+realoff[1])]],\
+                     [hitcircleoverlay, [int(offset[0]+cs/2+realoff[0]), int(offset[1]+cs/2+realoff[1])]],\
+                     [big_boi, [int(offset[0]+realoff[0]-cs*size/2)+cs, int(offset[1]+realoff[1]-cs*size/2)+cs]],\
+                     [number, [int(offset[0]+(3*cs/8)+cs/2+realoff[0]), int(offset[1]+(76*cs/256)+cs/2+realoff[1])]],\
+                     [hitcircle, [int(pos[0]-cs/2+realoff[0]),int(pos[1]-cs/2+realoff[1])]],\
+                     [hitcircleoverlay, [int(pos[0]-cs/2+realoff[0]),int(pos[1]-cs/2+realoff[1])]]\
                  ]]
         screen.fill((255, 255, 255, alpha), special_flags=pg.BLEND_RGBA_MULT)
-        window.blit(screen, [0,0])
+        return [window.blit(screen, [self.x-offset[0]-realoff[0], self.y-offset[1]-realoff[1]])]
